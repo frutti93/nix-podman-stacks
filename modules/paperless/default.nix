7abea4e4c017d04385c8d148d552ebe2008de589
Niklas Tasler <niklastasler@gmail.com>
2025-06-27 22:40:50 +0200

    refactor(paperless): refer to serviceDomain config option

--- modules/paperless/default.nix
+++ modules/paperless/default.nix
@@ -58,7 +58,7 @@ in {
           PAPERLESS_TIME_ZONE = config.tarow.podman.defaultTz;
           PAPERLESS_OCR_LANGUAGE = "deu";
           PAPERLESS_FILENAME_FORMAT = "{{created_year}}/{{correspondent}}/{{title}}";
-          PAPERLESS_URL = config.tarow.podman.stacks.traefik.serviceDomain;
+          PAPERLESS_URL = config.services.podman.containers.${name}.traefik.serviceDomain;
         };
         environmentFile = [cfg.envFile];
         port = 8000;

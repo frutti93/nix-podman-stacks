f14ac046a77032d3726571fafbce2ed13d3a1548
Niklas Tasler <niklastasler@gmail.com>
2025-07-05 11:38:35 +0200

    feat: add pocketid traefik middleware

--- modules/pocket-id/default.nix
+++ modules/pocket-id/default.nix
@@ -20,9 +20,45 @@ in {
         for a full list of available variables
       '';
     };
+    traefik = {
+      envFile = lib.mkOption {
+        type = lib.types.nullOr lib.types.path;
+        default = null;
+        description = ''
+          Environment file being passed to the Traefik container.
+          If this is set, a 'pocketid' middleware will be registered in Traefik
+          In order to work, the environment file should contain the secrets
+          'POCKET_ID_CLIENT_ID', 'POCKET_ID_CLIENT_SECRET' & 'OIDC_MIDDLEWARE_SECRET'
+        '';
+      };
+    };
   };
 
   config = lib.mkIf cfg.enable {
+    tarow.podman.stacks.traefik = lib.mkIf (cfg.traefik.envFile != null) {
+      containers.traefik = {
+        # Add host gateway route for pocket id domain, so that the oidc middleware can connect to it
+        extraConfig.Container.AddHost = "${cfg.containers.${name}.traefik.serviceHost}:host-gateway";
+        environmentFile = [cfg.traefik.envFile];
+      };
+
+      staticConfig.experimental.plugins.traefik-oidc-auth = {
+        moduleName = "github.com/sevensolutions/traefik-oidc-auth";
+        version = "v0.13.0";
+      };
+      dynamicConfig.http.middlewares = {
+        pocketid.plugin.traefik-oidc-auth = {
+          Secret = ''{{env "OIDC_MIDDLEWARE_SECRET"}}'';
+          Provider = {
+            Url = "http://${name}:1411";
+            ClientId = ''{{env "POCKET_ID_CLIENT_ID"}}'';
+            ClientSecret = ''{{env "POCKET_ID_CLIENT_SECRET"}}'';
+          };
+          Scopes = ["openid" "profile" "email"];
+        };
+      };
+    };
+
     services.podman.containers.${name} = {
       image = "ghcr.io/pocket-id/pocket-id:v1";
       volumes = [

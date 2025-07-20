5a99894c16b0bd38762078913422e45509170185
Niklas Tasler <niklastasler@gmail.com>
2025-07-20 17:16:18 +0200

    feat: migrate to wg-easy v15

--- modules/wg-easy/default.nix
+++ modules/wg-easy/default.nix
@@ -16,6 +16,9 @@ in {
       description = ''
         The external domain or IP address of the Wireguard server.
         Will be used as the 'endpoint' when generating client configurations.
+
+        Only has an effect during initial setup.
+        See <https://wg-easy.github.io/wg-easy/v15.1/advanced/config/unattended-setup/>
       '';
       default =
         if config.tarow.podman.stacks.traefik.enable
@@ -23,12 +26,21 @@ in {
         else config.tarow.podman.hostIP4Address;
       defaultText = lib.literalExpression ''"vpn.${config.tarow.podman.stacks.traefik.domain}"'';
     };
+    port = lib.mkOption {
+      type = lib.types.port;
+      description = ''
+        The port on which the Wireguard server will listen.
+        Will be passed as INIT_PORT during initial setup.
+        Only has an effect during initial setup.
+        See <https://wg-easy.github.io/wg-easy/v15.1/advanced/config/unattended-setup/>
+      '';
+      default = 51825;
+    };
     envFile = lib.mkOption {
       type = lib.types.nullOr lib.types.path;
       description = ''
         Path to the environment file.
-        Can be used to pass secrets, e.g. 'PASSWORD_HASH'.
-
+        Can be used to pass secrets, e.g. 'INIT_PASSWORD'.
       '';
       default = null;
     };
@@ -36,22 +48,29 @@ in {
 
   config = lib.mkIf cfg.enable {
     services.podman.containers.${name} = {
-      image = "ghcr.io/wg-easy/wg-easy:latest";
+      image = "ghcr.io/wg-easy/wg-easy:15";
       volumes = [
         "${storage}/config:/etc/wireguard"
       ];
 
-      ports = ["51820:51820/udp"];
+      ports = ["${toString cfg.port}:${toString cfg.port}/udp"];
       addCapabilities = ["NET_ADMIN" "NET_RAW" "SYS_MODULE"];
       extraPodmanArgs = [
         "--sysctl=net.ipv4.conf.all.src_valid_mark=1"
         "--sysctl=net.ipv4.ip_forward=1"
+        "--sysctl=net.ipv6.conf.all.disable_ipv6=0"
+        "--sysctl=net.ipv6.conf.all.forwarding=1"
+        "--sysctl=net.ipv6.conf.default.forwarding=1"
       ];
       environmentFile = lib.optional (cfg.envFile != null) cfg.envFile;
       environment = {
-        WG_HOST = cfg.host;
-        WG_DEFAULT_DNS = "1.1.1.1";
-        WG_DEFAULT_ADDRESS = "172.20.0.x";
+        INIT_ENABLED = true;
+        INIT_HOST = cfg.host;
+        INIT_USERNAME = "admin";
+        INIT_PORT = cfg.port;
+        INIT_DNS = "1.1.1.1,8.8.8.8";
+        INIT_IPV4_CIDR = "172.20.0.0/24";
+        INIT_IPV6_CIDR = "2001:0DB8::/32";
       };
 
       port = 51821;

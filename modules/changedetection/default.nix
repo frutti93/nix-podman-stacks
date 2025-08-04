ba965bc47fd1344f3022c869c1938ccf07d6bf40
Niklas Tasler <niklastasler@gmail.com>
2025-08-04 22:05:39 +0200

    refactor: changedetection image tag

--- modules/changedetection/default.nix
+++ modules/changedetection/default.nix
@@ -14,7 +14,7 @@ in {
   config = lib.mkIf cfg.enable {
     services.podman.containers = {
       ${name} = {
-        image = "ghcr.io/dgtlmoon/changedetection.io:0.50";
+        image = "ghcr.io/dgtlmoon/changedetection.io:0.50.8";
         volumes = [
           "${storage}:/datastore"
         ];

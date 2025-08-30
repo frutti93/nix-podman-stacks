65d21ed2a74c9a7453f90b7c1cdf9d00799cbf05
Niklas Tasler <niklastasler@gmail.com>
2025-08-30 18:54:42 +0200

    feat(audiobookshelf): add oidc groupname options

--- modules/audiobookshelf/default.nix
+++ modules/audiobookshelf/default.nix
@@ -36,24 +36,69 @@ in {
           <https://www.authelia.com/integration/openid-connect/frequently-asked-questions/#client-secret>
         '';
       };
+      adminGroup = lib.mkOption {
+        type = lib.types.str;
+        default = "${name}_admin";
+        description = ''
+          Users of this group will be assigned admin rights.
+
+          In order to take effect, you will have to enter the value `abs_groups` in the Group Claim form field in the Audiobookshelf UI.
+        '';
+      };
+      userGroup = lib.mkOption {
+        type = lib.types.str;
+        default = "${name}_user";
+        description = ''
+          Users of this group will be able to log in
+
+          In order to take effect, you will have to enter the value `abs_groups` in the Group Claim form field in the Audiobookshelf UI.
+        '';
+      };
     };
   };
 
   config = lib.mkIf cfg.enable {
-    nps.stacks.authelia.oidc.clients.audiobookshelf = lib.mkIf cfg.oidc.registerClient {
-      client_name = "Audiobookshelf";
-      client_secret = cfg.oidc.clientSecretHash;
-      public = false;
-      authorization_policy = config.nps.stacks.authelia.defaultAllowPolicy;
-      require_pkce = true;
-      pkce_challenge_method = "S256";
-      pre_configured_consent_duration = config.nps.stacks.authelia.oidc.defaultConsentDuration;
-      redirect_uris = [
-        "${cfg.containers.${name}.traefik.serviceUrl}/auth/openid/callback"
-        "${cfg.containers.${name}.traefik.serviceUrl}/auth/openid/mobile-redirect"
-        "audiobookshelf://oauth"
-      ];
+    nps.stacks.lldap.bootstrap.groups = lib.mkIf cfg.oidc.registerClient {
+      ${cfg.oidc.adminGroup} = {};
+      ${cfg.oidc.userGroup} = {};
     };
+    nps.stacks.authelia = let
+      customClaimName = "abs_groups";
+    in
+      lib.mkIf cfg.oidc.registerClient {
+        oidc.clients.audiobookshelf = {
+          client_name = "Audiobookshelf";
+          client_secret = cfg.oidc.clientSecretHash;
+          public = false;
+          authorization_policy = config.nps.stacks.authelia.defaultAllowPolicy;
+          require_pkce = true;
+          pkce_challenge_method = "S256";
+          pre_configured_consent_duration = config.nps.stacks.authelia.oidc.defaultConsentDuration;
+          redirect_uris = [
+            "${cfg.containers.${name}.traefik.serviceUrl}/auth/openid/callback"
+            "${cfg.containers.${name}.traefik.serviceUrl}/auth/openid/mobile-redirect"
+            "audiobookshelf://oauth"
+          ];
+          scopes = [
+            "openid"
+            "profile"
+            "email"
+            # Claim-Name has to be same as scope name
+            # See <https://github.com/advplyr/audiobookshelf/issues/3006>
+            customClaimName
+          ];
+          claims_policy = name;
+        };
+        settings.identity_providers.oidc = {
+          claims_policies.${name}.custom_claims = {
+            ${customClaimName}.attribute = customClaimName;
+          };
+          scopes.${customClaimName}.claims = [
+            customClaimName
+          ];
+        };
+        settings.definitions.user_attributes.${customClaimName}.expression = ''"${cfg.oidc.adminGroup}" in groups ? ["admin"] : ("${cfg.oidc.userGroup}" in groups ? ["user"] : [])'';
+      };
 
     services.podman.containers.${name} = {
       image = "ghcr.io/advplyr/audiobookshelf:2.29.0";

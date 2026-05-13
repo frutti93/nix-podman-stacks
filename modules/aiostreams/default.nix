d8fad5c820b597f159f2ed7301d2139846f452e4
Niklas Tasler <niklastasler@gmail.com>
2026-05-13 11:50:18 +0200

    fix: docs link

--- modules/aiostreams/default.nix
+++ modules/aiostreams/default.nix
@@ -21,7 +21,7 @@ in {
         Path to the file containing the secret key. Must be a 64-character hex string.
         Can be generated using `openssl rand -hex 32`
 
-        See <https://github.com/outline/outline/blob/main/.env.sample>
+        See <https://docs.aiostreams.viren070.me/configuration/environment-variables/
       '';
     };
     extraEnv = lib.mkOption {
@@ -31,7 +31,7 @@ in {
         Extra environment variables to set for the container.
         Can be used to pass secrets such as the `TMDB_ACCESS_TOKEN`.
 
-        See <https://github.com/Viren070/AIOStreams/wiki/Configuration>
+        See <https://docs.aiostreams.viren070.me/configuration/environment-variables/>
       '';
       example = {
         TMDB_ACCESS_TOKEN = {

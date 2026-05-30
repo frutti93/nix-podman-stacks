0214446013b05603cc7be1e68eb1991c1e6aa427
Niklas Tasler <niklastasler@gmail.com>
2026-05-30 12:15:44 +0200

    fix: reactive resume user

--- modules/reactive-resume/default.nix
+++ modules/reactive-resume/default.nix
@@ -114,6 +114,7 @@ in {
     services.podman.containers = {
       ${name} = {
         image = "ghcr.io/amruthpillai/reactive-resume:v5.1.7";
+        user = "${toString config.nps.defaultUid}:${toString config.nps.defaultGid}";
         volumeMap = {
           data = "${storage}/data:/app/data";
         };
